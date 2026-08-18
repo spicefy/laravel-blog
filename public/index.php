88d26f68228d0151d79ed20a5ab5aef08f9839b2
Spicefy <mcbenard@gmail.com>
2026-08-18 04:23:45 +0300

    reupload

--- public/index.php
+++ public/index.php
@@ -5,16 +5,33 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
+/*
+|--------------------------------------------------------------------------
+| Maintenance Mode Check
+|--------------------------------------------------------------------------
+*/
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
+/*
+|--------------------------------------------------------------------------
+| Register Composer Autoloader
+|--------------------------------------------------------------------------
+*/
 require __DIR__.'/../vendor/autoload.php';
 
-// Bootstrap Laravel and handle the request...
+/*
+|--------------------------------------------------------------------------
+| Bootstrap Laravel Application
+|--------------------------------------------------------------------------
+*/
 /** @var Application $app */
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+/*
+|--------------------------------------------------------------------------
+| Handle Request
+|--------------------------------------------------------------------------
+*/
 $app->handleRequest(Request::capture());
